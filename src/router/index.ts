5dfc9bc9fcbde163210b4b2af422aa794f15a3b1
codehuaa <1055928999@qq.com>
2022-07-01 15:31:53 +0800

    完成路由的权限判定

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,11 +7,31 @@ const routes: Array<RouteRecordRaw> = [
     name: 'login',
     component: () => import('../views/LoginView.vue')
   },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to) => {
+  let isAuth = true
+  if (!localStorage.getItem('token')) isAuth = false
+  /**
+   * 未登录用户前往非login页，强制跳转至login
+   * 已登录用户前往login页，返回上一页
+   * "/"与"/home"绑定
+   */
+  if (to.name != 'login' && !isAuth) return "/login"
+  if (to.name == 'login' && isAuth) history.back()
+  if (to.path == '/') return '/home'
+
+  return true;
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
